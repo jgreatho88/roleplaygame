152f4e44768631c03e26f9a9dc0dda9915ce21ea
John Greathouse <johngreathouse@johns-imac.local>
2016-08-07 13:17:44 -0500

    Initial Commit

---
+++ roleplaygame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  roleplaygame
+//
+//  Created by John Greathouse on 8/7/16.
+//  Copyright © 2016 John Greathouse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
