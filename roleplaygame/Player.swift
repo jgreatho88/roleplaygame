57fd50899bcd96cef41da2a4194ba04c5f0b5bc0
John Greathouse <johngreathouse@Johns-iMac.local>
2016-08-07 19:14:15 -0500

    lots more commits

--- roleplaygame/Player.swift
+++ roleplaygame/Player.swift
@@ -23,6 +23,12 @@ class Player: Character {
     var inventory: [String] {
         return _inventory
     }
+    
+    func addItemToInventory(item: String) {
+        _inventory.append(item)
+    
+    
+    }
 
     convenience init (name: String, hp: Int, attackPwr: Int) {
         self.init (startingHp: hp, attackPwr: attackPwr)
