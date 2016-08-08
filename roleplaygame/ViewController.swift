4e0be5f63d3f3797ed9d8f1113958f7f54b31ada
John Greathouse <johngreathouse@johns-imac.local>
2016-08-07 19:13:43 -0500

    committing on multiple adds

--- roleplaygame/ViewController.swift
+++ roleplaygame/ViewController.swift
@@ -21,15 +21,85 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var chestBtn: UIButton!
     
+    var player: Player!
+    var enemy: Enemy!
+    var chestMessage: String?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        player = Player(name: "DirtyLaundry21", hp: 110, attackPwr: 20)
+        generateRandomEnemy()
+        
+        playerHpLbl.text = "\(player.hp) HP"
+        
+    }
+    
+    func generateRandomEnemy() {
+        let rand = Int(arc4random_uniform(2))
+        
+        if rand == 0 {
+            enemy = Kimara(startingHp: 50, attackPwr: 12)
+        } else {
+            enemy = DevilWizard(startingHp: 60, attackPwr: 15)
+        }
+    
+    
     }
 
 
     @IBAction func onChestTapped(sender: AnyObject) {
         
+        chestBtn.hidden = true
+        printLbl.text = chestMessage
+        NSTimer.scheduledTimerWithTimeInterval(2.0, target: self, selector: #selector(ViewController.generateRandomEnemy), userInfo: nil, repeats: false)
+        
+        
     }
 
+    @IBAction func attackTapped(sender: AnyObject) {
+        if enemy.attemptAttack(player.attackPwr) {
+            printLbl.text = "Attacked \(enemy.type) for \(player.attackPwr) HP"
+        } else {
+            printLbl.text = "Attack was unsuccessful!"
+        }
+        
+        if let loot = enemy.dropLoot() {
+            player.addItemToInventory(loot)
+            chestMessage = "\(player.name) found \(loot)"
+            chestBtn.hidden = false
+        }
+        
+        if !enemy.isAlive {
+            enemyHpLbl.text = ""
+            printLbl.text = "Killed \(enemy.type)"
+            enemyImg.hidden = true
+        
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
